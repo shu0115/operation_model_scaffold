500bd0446596017432d21387a085370d1b4995d5
shu0115 <s.matsumoto0115@gmail.com>
2025-05-23 01:58:06 +0900

    Fix

--- lib/generators/operation_model/scaffold_generator.rb
+++ lib/generators/operation_model/scaffold_generator.rb
@@ -8,8 +8,28 @@ module OperationModel
       empty_directory "app/models/operations"
       empty_directory "app/models/operations/use_cases"
 
-      template "README.md",  "app/models/operations/README.md"
-      template "utility.rb", "app/models/operations/use_cases/utility.rb"
+      operation_paths = [
+        { template_name: "README.md",  file_path: "app/models/operations/README.md" },
+        { template_name: "utility.rb", file_path: "app/models/operations/use_cases/utility.rb" },
+      ]
+
+      operation_paths.each do |operation_path|
+        File.exist?(operation_path[:file_path]) ? say_status("skip", operation_path[:file_path], :yellow) : template(operation_path[:template_name], operation_path[:file_path])
+      end
+    end
+
+    def create_model_operations
+      skip_model_names = ['application_record', 'utility']
+      model_files      = Dir.glob("app/models/*.rb")
+
+      model_files.each do |model_file|
+        @model_name   = File.basename(model_file, ".rb")
+
+        template_name = "operation_model.rb.tt"
+        file_path     = "app/models/operations/#{@model_name}.rb"
+
+        (File.exist?(file_path) || skip_model_names.include?(@model_name)) ? say_status("skip", file_path, :yellow) : template(template_name, file_path)
+      end
     end
   end
 end
